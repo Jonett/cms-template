2e197b1be1ee4faf5fa2f497e180551b652ddb88
Joni <joni.hell@outlook.com>
2021-10-23 21:21:28 +0300

    Update dashboardController.php
    removed printings

--- app/controllers/dashboardController.php
+++ app/controllers/dashboardController.php
@@ -17,12 +17,6 @@ class dashboardController extends dashboardModel implements cms_views
     {
         $this->f3->set('content', 'dashboard/dashboard.html');
         echo \Template::instance()->render('/layout.html');
-        print_r('<pre>');
-        print_r($this->f3->get('SESSION.user_data')->get('firstname'));
-        print_r('</pre>');
-        print_r('<pre class="card"><div class"card-body">');
-        print_r($this->f3->get('DEV'));
-        print_r('</div><pre>');
     }
 
 }
